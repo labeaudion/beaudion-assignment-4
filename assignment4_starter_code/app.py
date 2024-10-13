2bf7a3cc8fe107e1c162a71198e05b8c6fd51f13
Lily Beaudion <beaudion@bu.edu>
2024-10-13 14:47:36 -0400

    making changes to stop words

--- assignment4_starter_code/app.py
+++ assignment4_starter_code/app.py
@@ -18,8 +18,11 @@ app = Flask(__name__)
 newsgroups = fetch_20newsgroups(subset='all')
 documents = newsgroups.data
 
+# getting NLTK stopwords
+stop_words = set(stopwords.words('english'))
+
 # initializing vectorizer
-vectorizer = TfidfVectorizer(stop_words='english')
+vectorizer = TfidfVectorizer(stop_words=stop_words)
 tfidf_matrix = vectorizer.fit_transform(documents)
 
 # defining LSA
@@ -27,16 +30,14 @@ n_components = 100  # You can adjust this as needed
 svd = TruncatedSVD(n_components=n_components)
 
 # LSA function
-def perform_lsa(tfidf_matrix, n_components=2):
+def perform_lsa(tfidf_matrix):
     lsa_matrix = svd.fit_transform(tfidf_matrix)
-    
     terms = vectorizer.get_feature_names_out()
     components = svd.components_
-    
     return lsa_matrix, terms, components
 
 # performing LSA
-lsa_matrix, terms, components = perform_lsa(tfidf_matrix, n_components)
+lsa_matrix, terms, components = perform_lsa(tfidf_matrix)
 
 
 def search_engine(query):
@@ -48,19 +49,14 @@ def search_engine(query):
     # TODO: Implement search engine here
     # return documents, similarities, indices 
 
-    # Step 1: Transform the query into TF-IDF representation
     query_tfidf = vectorizer.transform([query])
     
-    # Step 2: Reduce the dimensionality of the query using SVD
     query_lsa = svd.transform(query_tfidf)
     
-    # Step 3: Compute cosine similarity between the query and document LSA representations
     similarities = cosine_similarity(query_lsa, lsa_matrix).flatten()
     
-    # Step 4: Get the indices of the top 5 most similar documents
     top_indices = similarities.argsort()[-5:][::-1]
     
-    # Step 5: Retrieve the similar documents and their scores
     similar_documents = [documents[i] for i in top_indices]
     similar_scores = [similarities[i] for i in top_indices]
     
